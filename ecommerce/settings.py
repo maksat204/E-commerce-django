79e50eb134532f4c8a775baf8a861e4c1a1b610a
maksat204 <maxat.seitmaganbetov@narxoz.kz>
2023-02-26 20:32:21 +0600

    task4

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=@u@^b*ua_lguuw3#=x$&e#oh6qg%c$iec@2cdx(bsk_!+2ed(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -122,5 +122,9 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- joblists/models.py
+++ joblists/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+class joblists(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photo/%Y/%m/%d")
+    time_create = models.DateTimeField(auto_now_add=True)0
+    is_published = models.BooleanField(default=True)
 
-# Create your models here.
+
